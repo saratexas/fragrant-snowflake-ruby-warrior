ef6e025c57fe23da967cb6fd87f19c18b9cb7fb8
saratexas <sara.hudson@gmail.com>
2014-09-20 13:39:48 -0500

    Update level3.rb

--- level3.rb
+++ level3.rb
@@ -1,2 +1,17 @@
-fragrant-snowflake-ruby-warrior
-===============================
+
+class Player
+  def play_turn(warrior)
+    @health ||= warrior.health
+    case 
+    when warrior.health <=14 &&  warrior.feel.empty? && warrior.health==@health
+     warrior.rest! 
+    when warrior.feel.empty? && warrior.health< @health
+      warrior.walk!
+    when warrior.feel.empty? 
+      warrior.walk!
+    else
+      warrior.attack!
+    end 
+    @health= warrior.health
+  end
+end
